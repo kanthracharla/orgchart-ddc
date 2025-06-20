c74430f45ff9a08beacf05c8428479c9ee9a7c9f
kanthracharla <kanthracharla@gmail.com>
2025-06-19 23:13:30 -0400

    Update messagingUtil.js

--- messagingUtil.js
+++ messagingUtil.js
@@ -1,6 +1,25 @@
-window._va_data_received = false;
-function extractDataFromMessage(event) {
-    if (!event || !event.data) return null;
-    window._va_data_received = true;
-    return event.data.data || [];
-}
+// messagingUtil.js
+(function () {
+  if (window.va && window.va.messagingUtil) return;
+
+  window.va = window.va || {};
+  window.va.messagingUtil = {
+    setOnDataReceivedCallback: function (callback) {
+      console.log("[va.messagingUtil] Listening for postMessage events...");
+
+      function listener(event) {
+        if (event && event.data && event.data.data) {
+          console.log("[va.messagingUtil] Data received:", event.data);
+          callback(event.data);
+        }
+      }
+
+      window.addEventListener("message", listener, false);
+    },
+
+    postInitializationMessage: function () {
+      console.log("[va.messagingUtil] Posting initialization message");
+      window.parent.postMessage({ type: "initialized" }, "*");
+    }
+  };
+})();
